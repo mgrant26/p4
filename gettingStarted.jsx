6b6c4ab0045453ce42a2771a6533e3d56816c046
Teh Li Peh <97926241+TehLiPeh@users.noreply.github.com>
2023-10-02 15:35:37 -0400

    updated assignment

--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/main.css';
 
 import Example from './components/example/Example';
+import Header from "./components/header/Header";
 
 ReactDOM.render(
   <Example />,
