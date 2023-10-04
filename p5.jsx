3ef1ad8aba6f09842ced8a7b5ea6a9a71cfb5c83
Teh Li Peh <97926241+TehLiPeh@users.noreply.github.com>
2023-10-03 20:09:24 -0400

    Update p5.jsx

--- p5.jsx
+++ p5.jsx
@@ -23,33 +23,33 @@ ReactDOM.render(
     document.getElementById('reactapp')
 );
 
-// class Toggle extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {isToggleOn: true};
-//         this.handleClick = this.handleClick.bind(this);
-//     }
-//
-//     handleClick() {
-//         this.setState(prevState => ({
-//             isToggleOn: !prevState.isToggleOn
-//         }));
-//     }
-//
-//     render() {
-//         return (
-//             <div className="container">
-//                 <button onClick={this.handleClick}>
-//                     {this.state.isToggleOn ? 'Switch to States' : 'Switch to Example'}
-//                 </button>
-//                 <hr />
-//                 {this.state.isToggleOn ? <Example /> : <States />}
-//             </div>
-//         );
-//     }
-// }
-//
-// ReactDOM.render(
-//     <Toggle />,
-//     document.getElementById('reactapp')
-// );
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <button onClick={this.handleClick}>
+                    {this.state.isToggleOn ? 'Switch to States' : 'Switch to Example'}
+                </button>
+                <hr />
+                {this.state.isToggleOn ? <Example /> : <States />}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Toggle />,
+    document.getElementById('reactapp')
+);
